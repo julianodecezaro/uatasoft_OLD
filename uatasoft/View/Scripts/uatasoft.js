141891c9f7f698cbd1b4a440a61bdfed8e8559b6
julianodecezaro <julianodecezaro@gmail.com>
2013-04-29 17:58:38 -0300

    Criacao menu

--- uatasoft/View/Scripts/uatasoft.js
+++ uatasoft/View/Scripts/uatasoft.js
@@ -13,6 +13,32 @@ $(document).ready(function(){
 	$('#nav4').click(function(){ _menu(4); });
 	$('#nav5').click(function(){ _menu(5); });
 
+	$('#nav1').hover(function(){ hoverSrc(this); });
+	$('#nav2').hover(function(){ hoverSrc(this); });
+	$('#nav3').hover(function(){ hoverSrc(this); });
+	$('#nav4').hover(function(){ hoverSrc(this); });
+	$('#nav5').hover(function(){ hoverSrc(this); });
+
+	function hoverSrc(_this){
+		var parse = '.png';
+		var _src = $(_this).attr('src');
+		var sp = _src.split(parse);
+		if(!$(_this).attr('ishover')){
+			$(_this).attr('src', sp[0]+'_hover'+parse);
+			$(_this).attr('ishover', 'true');
+		}
+	}
+
+	function houtSrc(_this){
+		var parse = '.png';
+		var _src = $(_this).attr('src');
+		var sp = _src.split(parse);
+		if(!$(_this).attr('ishover')){
+			$(_this).attr('src', sp[0]+'_hover'+parse);
+			$(_this).attr('ishover', 'true');
+		}
+	}
+
 	function _menu(_id){
 		$('section .container').hide();
 		$('#content'+_id).show(0, function(){
